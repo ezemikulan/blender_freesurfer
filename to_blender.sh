e6eb0a4fc004177d8be459feb09946949186c368
Alex Rockhill <aprockhill206@gmail.com>
2019-03-14 10:14:16 -0400

    added flash compatibility

--- to_blender.sh
+++ to_blender.sh
@@ -6,7 +6,7 @@ fx_dir=$3 # directory with Brainder conversion functions (3rd input parameter)
 
 echo "Processing subject: ${subj}"
 
-dir_bem=${subj_dir}/${subj}/bem/watershed
+dir_bem=${subj_dir}/${subj}/bem
 dir_conv=${dir_bem}/conv
 
 if [ ! -d "$dir_conv" ]; then
@@ -17,11 +17,23 @@ surfs=(brain inner_skull outer_skull outer_skin)
 
 for s in "${surfs[@]}"
 do
-	surf_ori=${subj}_${s}_surface
-	surf_bkup=${dir_conv}/${surf_ori}_bkup
+        if [ -f "${dir_bem}/watershed/${subj}_${s}_surface" ]; then
+            dir_ori=watershed
+            surf_ori=${subj}_${s}_surface
+        elif [ -f "${dir_bem}/flash/${s}.surf" ]; then
+            dir_ori=flash
+            surf_ori=${s}.surf
+        else
+            echo "No water shed or flash file found for: ${s} "
+            echo "under ${dir_bem}/watershed/${subj}_${s}_surface "
+            echo "or ${dir_bem}/flash/${s}.surf" 
+            continue
+        fi
+        surf_bkup=${dir_conv}/${surf_ori}_bkup
+	
 
 	if [ ! -f "$surf_bkup" ]; then
-	  cp ${dir_bem}/${surf_ori} ${surf_bkup} # make backup file if it doesn't exist
+	  cp ${dir_bem}/${dir_ori}/${surf_ori} ${surf_bkup} # make backup file if it doesn't exist
 	fi
 
 	mris_convert ${surf_bkup} ${dir_conv}/${surf_ori}.asc   # convert to ascii
